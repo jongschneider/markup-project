8b180650fe69e6bc90a8b83598d759e3cd3741f1
Jonathan Schneider <jongschneider@gmail.com>
2017-08-28 09:06:58 -0400

    set up a 'hello world' to test express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
 const express = require('express');
+const PORT = 3000;
+const app = express();
+
+app.get('/', (req, res, next) => {
+	res.end('Its working!');
+});
+
+app.listen(PORT, () => `Now listening on port ${PORT}`);
