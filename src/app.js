9f541716abe218b9cd564c52eb825174fd015c7f
Jonathan Schneider <jongschneider@gmail.com>
2017-08-28 10:50:56 -0400

    added a ton of dependencies. moves routes to index.js in routes directory. Set template engine as pug. set up static file path for stylesheet. Added error handling middleware.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,44 @@
 const express = require('express');
-const PORT = 3000;
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const routes = require('./routes/index');
+const errorHandlers = require('./handlers/errorHandlers');
+
 const app = express();
+const port = process.env.PORT || 7777;
 
-app.get('/', (req, res, next) => {
-	res.end('Its working!');
+// view engine setup
+app.set('views', path.join(__dirname, 'views')); // this is the folder where we keep our pug files
+app.set('view engine', 'pug'); // we use the engine pug, mustache or EJS work great too
+
+// serves up static files from the public folder. Anything in public/ will just be served up as the file it is
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Takes the raw requests and turns them into usable properties on req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// After allllll that above middleware, we finally handle our own routes!
+app.use('/', routes);
+
+// If that above routes didnt work, we 404 them and forward to error handler
+app.use(errorHandlers.notFound);
+
+// One of our error handlers will see if these errors are just validation errors
+app.use(errorHandlers.flashValidationErrors);
+
+// Otherwise this was a really bad error we didn't expect! Shoot eh
+if (app.get('env') === 'development') {
+	/* Development Error Handler - Prints stack trace */
+	app.use(errorHandlers.developmentErrors);
+}
+
+// production error handler
+app.use(errorHandlers.productionErrors);
+
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () => {
+	console.log(`Express running → PORT ${server.address().port}`);
 });
-
-app.listen(PORT, () => `Now listening on port ${PORT}`);
