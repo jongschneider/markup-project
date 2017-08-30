1e38eeeb8ee8a407a0977d4dd8bfff6ae35df45d
Jonathan Schneider <jongschneider@gmail.com>
2017-08-30 11:18:30 -0400

    added mysql dependencies to index.js and moved the /getscore route here as well. Currently working.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,34 @@
 
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
+const { dbConfig } = require('../handlers/config');
 
+// create mySQL connection
+const db = mysql.createConnection(dbConfig);
+
+// connect to db
+db.connect(err => {
+	if (err) {
+		throw err;
+	}
+	console.log('MySql connected.');
+});
+
+//homepage
 router.get('/', (req, res, next) => {
 	res.render('index');
+});
+
+//
+router.get('/getscore', (req, res, next) => {
+	let sql =
+		'SELECT * FROM scores WHERE html_filenames_html_keys_html_keyname="bob"';
+	let query = db.query(sql, (err, result) => {
+		if (err) throw err;
+		console.log(result);
+		res.end(JSON.stringify(result));
+	});
 });
 
 module.exports = router;
