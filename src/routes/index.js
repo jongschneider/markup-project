cd355d7d979907afdd06ee9c565297a1b03808bd
Jonathan Schneider <jongschneider@gmail.com>
2017-08-30 22:12:14 -0400

    rearranged routes to place working, commented routes a the top and test rout at the bottom.

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,21 +9,12 @@ const dbControllers = require('../controllers/dbControllers');
 // homepage get list of files in data folder
 router.get('/', catchErrors(dbControllers.homePage));
 
-// homepage get html from a file
+// homepage displays html from file scored
 router.get(
 	'/scoreFile',
 	dbControllers.scoreAndUpdate,
 	catchErrors(dbControllers.showHtml)
 );
-
-router.get('/getscore', (req, res, next) => {
-	let sql =
-		'SELECT * FROM scores WHERE html_filenames_html_keys_html_keyname="bob"';
-	let query = db.query(sql, (err, result) => {
-		if (err) throw err;
-		res.end(JSON.stringify(result));
-	});
-});
 
 // homepage get html from a file
 router.get('/getHtml', async (req, res, next) => {
@@ -35,6 +26,16 @@ router.get('/getHtml', async (req, res, next) => {
 		data: fileList,
 		html,
 		fileName: req.query.select
+	});
+});
+
+//test get route
+router.get('/getscore', (req, res, next) => {
+	let sql =
+		'SELECT * FROM scores WHERE html_filenames_html_keys_html_keyname="bob"';
+	let query = db.query(sql, (err, result) => {
+		if (err) throw err;
+		res.end(JSON.stringify(result));
 	});
 });
 
