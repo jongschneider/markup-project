f914e0eab90d6f215b41a06e45e38ae8a4d0cea2
Jonathan Schneider <jongschneider@gmail.com>
2017-09-10 13:25:19 -0400

    messed around with error handlers to see which ones weren't required

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use('/', routes);
 app.use(errorHandlers.notFound);
 
 // One of our error handlers will see if these errors are just validation errors
-app.use(errorHandlers.flashValidationErrors);
+// app.use(errorHandlers.flashValidationErrors);
 
 // Otherwise this was a really bad error we didn't expect! Shoot eh
 if (app.get('env') === 'development') {
