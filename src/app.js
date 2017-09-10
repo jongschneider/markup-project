6eda8fb6e6087f34a418deff98b2a0e0b539a9e4
Jonathan Schneider <jongschneider@gmail.com>
2017-09-10 13:37:56 -0400

    removed commented out unused error handlers

--- src/app.js
+++ src/app.js
@@ -28,9 +28,6 @@ app.use('/', routes);
 // If that above routes didnt work, we 404 them and forward to error handler
 app.use(errorHandlers.notFound);
 
-// One of our error handlers will see if these errors are just validation errors
-// app.use(errorHandlers.flashValidationErrors);
-
 // Otherwise this was a really bad error we didn't expect! Shoot eh
 if (app.get('env') === 'development') {
 	/* Development Error Handler - Prints stack trace */
